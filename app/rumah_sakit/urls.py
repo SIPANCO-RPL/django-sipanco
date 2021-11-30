ecddf1d9a5f94d21198d86b5de5edc4181256596
Widyanto <widyantohadi@gmail.com>
2021-11-30 22:36:18 +0700

    malam2

--- app/rumah_sakit/urls.py
+++ app/rumah_sakit/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from .controller import *
 
-app_name = 'auth'
+app_name = 'rumah_sakit'
 
 urlpatterns = [
-    path('', lihat_rumah_sakit, name='lihat_rumah_sakit'),
+    path('/', lihat_rumah_sakit, name='lihat_rumah_sakit'),
+    path('/create', lihat_rumah_sakit, name='lihat_rumah_sakit'),
 ]
