06a350066a4b696d2954f02436afa5c2efbd8d65
advistasyam <advis.tasyah@ui.ac.id>
2021-11-30 23:22:06 +0700

    before conflict

--- app/rumah_sakit/urls.py
+++ app/rumah_sakit/urls.py
@@ -4,6 +4,6 @@ from .controller import *
 app_name = 'rumah_sakit'
 
 urlpatterns = [
-    path('/', lihat_rumah_sakit, name='lihat_rumah_sakit'),
-    path('/create', lihat_rumah_sakit, name='lihat_rumah_sakit'),
+    path('', lihat_rumah_sakit, name='lihat_rumah_sakit'),
+    path('create', lihat_rumah_sakit, name='lihat_rumah_sakit'),
 ]
