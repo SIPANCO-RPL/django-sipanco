fe0946207f9057e2175e299fd96c0dcade50a839
Widyanto <widyantohadi@gmail.com>
2021-11-30 16:40:10 +0700

    change folder structure

--- sipanco/loginapp/views.py
+++ sipanco/loginapp/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from sipanco.di import injector
 from .service import ILoginService
+from django.views.decorators.http import require_http_methods as methods
+
 
 login_service = injector.get(ILoginService)
 
+@methods(["GET"])
 def index(request):
     print(login_service.test())
     return render(request, 'index.html')
