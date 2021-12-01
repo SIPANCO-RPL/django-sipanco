0e38e934db159967e2356f31033a4b557900aa1e
Widyanto <widyantohadi@gmail.com>
2021-12-01 21:14:05 +0700

    add form and list reservasi vaksin

--- app/rumah_sakit/urls.py
+++ app/rumah_sakit/urls.py
@@ -5,8 +5,8 @@ app_name = 'rumah_sakit'
 
 urlpatterns = [
     path('', lihat_rumah_sakit, name='lihat_rumah_sakit'),
-    path('/rsui', lihat_jadwal_dokter, name='lihat_jadwal_dokter'),
-    path('/create-jadwal', create_jadwal_dokter, name='create_jadwal_dokter'),
-    path('/lihat-ruangan', lihat_ruangan, name='lihat_ruangan'),
-    path('/buat-ruangan', create_ruangan, name="buat_ruangan")
+    path('rsui', lihat_jadwal_dokter, name='lihat_jadwal_dokter'),
+    path('create-jadwal', create_jadwal_dokter, name='create_jadwal_dokter'),
+    path('lihat-ruangan', lihat_ruangan, name='lihat_ruangan'),
+    path('buat-ruangan', create_ruangan, name="buat_ruangan")
 ]
--- app/vaksin/urls.py
+++ app/vaksin/urls.py
@@ -4,6 +4,8 @@ from .controller import *
 app_name = 'vaksin'
 
 urlpatterns = [
-    path('/addVaksin', create_vaksin, name='create_vaksin'),
     path('', list_vaksin, name='list_vaksin'),
+    path('addVaksin', create_vaksin, name='create_vaksin'),
+    path('reservasi/list', list_reservasi_vaksin, name='list_reservasi_vaksin'),
+    path('reservasi/create', create_reservasi_vaksin, name='create_reservasi_vaksin'),
 ]
