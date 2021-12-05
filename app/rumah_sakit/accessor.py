baacebb1ef0cc154fc56eb73647366b606c02e86
Hasna Nadifah <nadifahsn21@gmail.com>
2021-12-05 09:58:12 +0700

    create appointment and list appointment

--- app/rumah_sakit/accessor.py
+++ app/rumah_sakit/accessor.py
@@ -1,6 +1,6 @@
 from typing import List, Union, Optional
 
-from app.rumah_sakit.models import Ruangan, RumahSakit
+from app.rumah_sakit.models import Ruangan, RumahSakit, AppointmentDokter, JadwalDokter
 
 
 class RumahSakitAccessor:
@@ -23,3 +23,23 @@ class RumahSakitAccessor:
 
     def get_ruangan_by_petugas(self, kode: str) -> List[Ruangan]:
         return Ruangan.objects.filter(rumahSakit__id = kode)
+    
+    def get_appointment_dokter(
+        self, pasien_id: Optional[int] = None
+    ) -> List[AppointmentDokter]:
+        queryset = AppointmentDokter.objects.all()
+
+        if pasien_id:
+            queryset.filter(pasien__id=pasien_id)
+
+        return queryset
+
+    def create_appointment(
+        self, dict_data: Dict[str, Any]
+    ) -> Optional[AppointmentDokter]:
+        try:
+            obj = AppointmentDokter(**dict_data)
+            obj.save()
+            return obj
+        except:
+            return None
