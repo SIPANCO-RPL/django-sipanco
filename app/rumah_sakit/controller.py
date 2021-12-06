c6311c8f1fe9a4b5999ebe92605262538c4aa14f
Muhamad Adamy Rayeuk <adamy.rayeuk@gmail.com>
2021-12-07 00:03:24 +0700

    Integrate ruangan backend with the frontend and fix issue in ruangan backend

--- app/rumah_sakit/controller.py
+++ app/rumah_sakit/controller.py
@@ -1,10 +1,12 @@
 from django.http.request import HttpRequest
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.urls import reverse
+from app.di import injector
 from django.views.decorators.http import require_http_methods as methods
 
 from app.rumah_sakit.service import RumahSakitService
 
+rumah_sakit_service = injector.get(RumahSakitService)
 
 def lihat_rumah_sakit(request):
     return render(request, 'rumah_sakit.html')
@@ -14,8 +16,8 @@ def lihat_jadwal_dokter(request):
 
 @methods(["GET"])
 def lihat_ruangan(request: HttpRequest):
-    context = {"list_ruangan": RumahSakitService.get_all_ruangan(request)}
-    return render(request, 'list_ruangan.html')
+    context = {"list_ruangan": rumah_sakit_service.get_all_ruangan(request)}
+    return render(request, 'list_ruangan.html', context)
 
 def create_jadwal_dokter(request):
     return render(request, 'create_jadwal_dokter.html')
@@ -23,24 +25,24 @@ def create_jadwal_dokter(request):
 @methods(["GET", "POST"])
 def create_ruangan(request: HttpRequest):
     if request.method == "POST":
-        ruangan = RumahSakitService.create_ruangan(request)
-        if not ruangan:
-            context = {"message": "Ruangan gagal dibuat"}
-            return render(request, 'list_ruangan.html', context)
-
-        return reverse(lihat_ruangan)
+        ruangan = rumah_sakit_service.create_ruangan(request)
+        if ruangan:
+            return redirect("/rumah-sakit/lihat-ruangan/")
+       
+        context = {"message": "Ruangan gagal dibuat"}
+        return render(request, 'list_ruangan.html', context)
 
     return render(request, 'create_ruangan.html')
 
 @methods(["GET"])
 def list_appointment(request: HttpRequest):
-    appointment_list = RumahSakitService.get_appointment_dokter(request)
+    appointment_list = rumah_sakit_service.get_appointment_dokter(request)
     return render(request, 'list_appointment.html', {'appointment_list': appointment_list})
 
 @methods(["GET", "POST"])
 def create_appointment(request: HttpRequest):
     if request.method == "POST":
-        reservasi = RumahSakitService.create_appointment_dokter(request)
+        reservasi = rumah_sakit_service.create_appointment_dokter(request)
         if not reservasi:
             context = {"message": "Gagal Membuat Reservasi"}
             return render(request, 'create_appointment.html', context)
--- app/rumah_sakit/urls.py
+++ app/rumah_sakit/urls.py
@@ -5,10 +5,10 @@ app_name = 'rumah_sakit'
 
 urlpatterns = [
     path('', lihat_rumah_sakit, name='lihat_rumah_sakit'),
-    path('rsui', lihat_jadwal_dokter, name='lihat_jadwal_dokter'),
-    path('create-jadwal', create_jadwal_dokter, name='create_jadwal_dokter'),
-    path('lihat-ruangan', lihat_ruangan, name='lihat_ruangan'),
-    path('buat-ruangan', create_ruangan, name="buat_ruangan"),
-    path('createappointment', create_appointment, name='create_appointment'),
-    path('listappointment', list_appointment, name='list_appointment'),
+    path('rsui/', lihat_jadwal_dokter, name='lihat_jadwal_dokter'),
+    path('create-jadwal/', create_jadwal_dokter, name='create_jadwal_dokter'),
+    path('lihat-ruangan/', lihat_ruangan, name='lihat_ruangan'),
+    path('buat-ruangan/', create_ruangan, name="buat_ruangan"),
+    path('createappointment/', create_appointment, name='create_appointment'),
+    path('listappointment/', list_appointment, name='list_appointment'),
 ]
