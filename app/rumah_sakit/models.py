26f056f577a9c638b33a0126347138f1cefda402
galacticor <widyantohadi@gmail.com>
2021-12-06 19:46:15 +0700

    fix circular import

--- app/rumah_sakit/models.py
+++ app/rumah_sakit/models.py
@@ -1,10 +1,13 @@
 from django.db import models
 from django.db.models.deletion import CASCADE
 
+from app.auth.models import Petugas
+
 class RumahSakit(models.Model):
     kode = models.CharField(max_length=64)
     nama = models.CharField(max_length=256)
     alamat = models.CharField(max_length=512)
+    petugas = models.OneToOneField(Petugas, on_delete=models.CASCADE, related_name="rumah_sakit", null=True)
 
     def __str__(self):
         return self.nama
